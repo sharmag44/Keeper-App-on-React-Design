ffdf1a87bbe2390b82f94342f6c6290b7a981ec2
sharmag44 <62098467+sharmag44@users.noreply.github.com>
2021-07-25 22:09:47 +0530

    Initial commit

---
+++ component/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1 className="h1">This is Title</h1>
+      <p className="p">This is Notes</p>
+    </div>
+  );
+}
+
+export default Note;
